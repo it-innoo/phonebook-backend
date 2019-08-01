ced9e8fa11efcfb92f1ea7751f0175390418fa1b
jailgreen <36865973+jailgreen@users.noreply.github.com>
2019-08-01 12:23:59 +0300

    Step 3

--- index.js
+++ index.js
@@ -45,6 +45,13 @@ app.get('/api/persons', (request, response) => {
   response.json(persons)
 })
 
+app.get('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  const name = persons.find(p => p.id === id)
+
+  name ? response.json(name) : response.status(404).end()
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
