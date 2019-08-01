773d10c7618b120d2800cb2a4563829ed56b8074
jailgreen <36865973+jailgreen@users.noreply.github.com>
2019-08-01 15:52:38 +0300

    Step 8

--- index.js
+++ index.js
@@ -4,7 +4,10 @@ const bodyParser = require('body-parser')
 const morgan = require('morgan')
 
 app.use(bodyParser.json())
-app.use(morgan('tiny'))
+morgan
+  .token('body', req => JSON.stringify(req.body))
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
+
 
 let persons = [
   {
